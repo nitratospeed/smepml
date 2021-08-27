5113125a40b77ce3bc37e3b154782e6c3c1e01f3
Eduardo Rojas <nitratospeed@gmail.com>
2021-08-27 16:24:37 -0500

    fix: removed unused pkgs, fix template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,26 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { FooterComponent } from './components/shared/footer/footer.component';
-import { HeaderComponent } from './components/shared/header/header.component';
 import { PacientesComponent } from './components/pacientes/pacientes.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { DiagnosticoComponent } from './components/diagnostico/diagnostico.component';
 import { AutocompleteLibModule } from 'angular-ng-autocomplete';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { DiagnosticoHistorialComponent } from './components/diagnostico-historial/diagnostico-historial.component';
-import { MainComponent } from './components/shared/main/main.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FooterComponent,
-    HeaderComponent,
     PacientesComponent,
     DashboardComponent,
     DiagnosticoComponent,
     DiagnosticoHistorialComponent,
-    MainComponent,
   ],
   imports: [
     BrowserModule,
